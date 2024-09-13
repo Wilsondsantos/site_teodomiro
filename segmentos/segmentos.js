dccc187e6b059bad54fcd01cbcf6fc213ee8286d
Gustavo144p <140839847+Gustavo144p@users.noreply.github.com>
2024-09-13 04:55:42 -0700

    subi_os_arquivos

---
+++ segmentos/segmentos.js
@@ -0,0 +1,10 @@
+// barra de navegação
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+//   barra de navegação
